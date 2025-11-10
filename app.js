8c339227b29aba422e70c7701c40c8b1295da9a9
The Lord of Darkness <Elcan@div.edu.az>
2025-11-11 02:42:29 +0400

    json

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const app = express()
 
 
 app.use(cors())
-
+app.use(express.json())
 
 app.get('/shafiqa-pro-max',(req, res)=>{
     res.json([{id: 1, name:"Shafiqa", sname:"Alakbarova", age:"18"}])
