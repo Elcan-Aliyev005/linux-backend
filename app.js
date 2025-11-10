ebd754c616d2dc3a69a94fedda67f31527b72e41
The Lord of Darkness <Elcan@div.edu.az>
2025-11-11 00:11:00 +0400

    first commit

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const cors = require('cors')
+const app = express()
+
+
+app.use(cors())
+
+
+app.get('/shafiqa-pro-max',(req, res)=>{
+    res.json([{id: 1, name:"Shafiqa", sname:"Alakbarova", age:"18"}])
+})
+
+
+
+
+
+app.listen(3000, ()=>{
+        console.log('Life is good');
+})
