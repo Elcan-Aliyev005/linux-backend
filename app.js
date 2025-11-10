8d06c6003875d7437579fa1a8e7610d9c7bef7fd
The Lord of Darkness <Elcan@div.edu.az>
2025-11-11 03:34:12 +0400

    image

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ app.use(cors())
 app.use(express.json())
 
 app.get('/shafiqa-pro-max',(req, res)=>{
-    res.json([{id: 1, name:"Shafiqa", sname:"Alakbarova", age:"18"}])
+    res.json([{id: 1, name:"Shafiqa", sname:"Alakbarova", age:"18",imageUrl:"https://media.tenor.com/OZad3V7-ZgwAAAAM/baby-gumball-gumball.gif"}])
 })
 
 
