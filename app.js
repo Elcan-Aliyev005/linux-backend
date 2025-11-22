5fce7d173597db023dd8be39522f30cbc2f184a2
The Lord of Darkness <Elcan@div.edu.az>
2025-11-23 01:15:32 +0400

    farizi elave etdik

--- app.js
+++ app.js
@@ -7,7 +7,11 @@ app.use(cors())
 app.use(express.json())
 
 app.get('/shafiqa-pro-max',(req, res)=>{
-    res.json([{id: 1, name:"Shafiqa", sname:"Alakbarova", age:"18",imageUrl:"https://m.media-amazon.com/images/M/MV5BNDI1YjIyN2EtMWExZC00ZjFjLWI0ODAtMWY0ODYyODBmNGEzXkEyXkFqcGc@._V1_.jpg"}])
+    res.json([{id: 1, name:"Shafiqa", sname:"Alakbarova", age:"19",imageUrl:"https://m.media-amazon.com/images/M/MV5BNDI1YjIyN2EtMWExZC00ZjFjLWI0ODAtMWY0ODYyODBmNGEzXkEyXkFqcGc@._V1_.jpg"}])
+})
+
+app.get('/fariz-pro-max',(req, res)=>{
+    res.json([{id: 1, name:"Fariz", sname:"Əsədzadə", age:"96",imageUrl:"https://static.wikia.nocookie.net/kungfupanda/images/c/ca/Spirit_Tai_Lung.jpg/revision/latest/scale-to-width/360?cb=20240314201219"}])
 })
 
 
